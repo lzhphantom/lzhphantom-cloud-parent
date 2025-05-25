ef8490e67046f061cbc40577da2eb71e869db78d
lzhphantom <987090402@qq.com>
2025-05-25 19:44:10 +0800

    build(docker): 更新项目版本并添加 Docker 镜像构建配置
    - 将 lzhphantom-cloud-parent 版本从 1.0.3 升级到 1.0.4
    - 更新相关子模块版本
    - 在 pom.xml 中添加 Docker 插件配置，用于构建 Docker 镜像
    - 替换 Swagger 注解为 OpenAPI v3 注解

--- lzhphantom-web/src/main/java/com/lzhphantom/common/web/controller/BaseController.java
+++ lzhphantom-web/src/main/java/com/lzhphantom/common/web/controller/BaseController.java
@@ -5,7 +5,7 @@ import com.lzhphantom.common.base.result.LzhphantomPage;
 import com.lzhphantom.common.base.result.LzhphantomResult;
 import com.lzhphantom.common.web.entity.BaseEntity;
 import com.lzhphantom.common.web.service.BaseService;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,7 +38,7 @@ public abstract class BaseController<S extends BaseService<E, C, U, Q, P, O>, E 
      * @param createDto 创建DTO
      * @return 创建结果
      */
-    @ApiOperation(value = "创建记录")
+    @Operation(summary = "创建记录")
     @PostMapping("/create")
     public LzhphantomResult<Long> create(@RequestBody C createDto) {
         return LzhphantomResult.ok(getService().create(createDto));
@@ -50,7 +50,7 @@ public abstract class BaseController<S extends BaseService<E, C, U, Q, P, O>, E 
      * @param updateDto 更新DTO
      * @return 更新结果
      */
-    @ApiOperation("更新记录")
+    @Operation(summary = "更新记录")
     @PostMapping("/update")
     public LzhphantomResult<Boolean> update(@RequestBody U updateDto) {
         return LzhphantomResult.ok(getService().update(updateDto));
@@ -62,7 +62,7 @@ public abstract class BaseController<S extends BaseService<E, C, U, Q, P, O>, E 
      * @param id 记录ID
      * @return 删除结果
      */
-    @ApiOperation("根据ID删除记录")
+    @Operation(summary = "根据ID删除记录")
     @GetMapping("/delete/{id}")
     public LzhphantomResult<Boolean> delete(@PathVariable("id") Long id) {
         return LzhphantomResult.ok(getService().deleteById(id));
@@ -74,7 +74,7 @@ public abstract class BaseController<S extends BaseService<E, C, U, Q, P, O>, E 
      * @param deleteDto 批量删除DTO
      * @return 删除结果
      */
-    @ApiOperation("批量删除记录")
+    @Operation(summary = "批量删除记录")
     @PostMapping("/batchDelete")
     public LzhphantomResult<Boolean> batchDelete(@RequestBody BatchDeleteDto<?> deleteDto) {
         return LzhphantomResult.ok(getService().batchDelete(deleteDto));
@@ -86,7 +86,7 @@ public abstract class BaseController<S extends BaseService<E, C, U, Q, P, O>, E 
      * @param id 记录ID
      * @return 详情DTO
      */
-    @ApiOperation("根据ID获取详情")
+    @Operation(summary = "根据ID获取详情")
     @GetMapping("/getById/{id}")
     public LzhphantomResult<O> getById(@PathVariable("id") Long id) {
         return LzhphantomResult.ok(getService().getOriginById(id));
@@ -98,7 +98,7 @@ public abstract class BaseController<S extends BaseService<E, C, U, Q, P, O>, E 
      * @param pageQueryDto 分页查询DTO
      * @return 分页结果
      */
-    @ApiOperation("分页查询")
+    @Operation(summary = "分页查询")
     @PostMapping("/getAllByPage")
     public LzhphantomResult<LzhphantomPage<P>> getAllByPage(@RequestBody Q pageQueryDto) {
         return LzhphantomResult.ok(getService().page(pageQueryDto));
